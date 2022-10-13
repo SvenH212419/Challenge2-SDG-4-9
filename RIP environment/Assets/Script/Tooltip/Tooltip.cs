5a9d56481fd06533d91d055facb21879aa122fb1
SvenH212419 <97597028+SvenH212419@users.noreply.github.com>
2022-10-13 14:50:24 +0200

    Tooltip WIP

---
+++ RIP environment/Assets/Script/Tooltip/Tooltip.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI headerField;
+
+    public TextMeshProUGUI contentField;
+
+    public LayoutElement layoutElement;
+
+    public int characterWrapLimit;
+
+    public void SetText(string content, string header = "")
+    {
+        if (string.IsNullOrEmpty(header))
+        {
+            headerField.gameObject.SetActive(false);
+        }
+        else 
+        { 
+            headerField.gameObject.SetActive(true);
+            headerField.text = header;
+        }
+
+        contentField.text = content;
+
+        int headerLengt = headerField.text.Length;
+        int contentLengt = contentField.text.Length;
+
+        layoutElement.enabled = (headerLengt > characterWrapLimit || contentLengt > characterWrapLimit) ? true : false;
+
+    }
+
+    private void Update()
+
+    {
+        if (Application.isEditor)
+        {
+            int headerLengt = headerField.text.Length;
+            int contentLengt = contentField.text.Length;
+
+            layoutElement.enabled = (headerLengt > characterWrapLimit || contentLengt > characterWrapLimit) ? true : false;
+        }
+    }
+}
